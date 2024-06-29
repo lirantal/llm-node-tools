873b283cdc01e407f19349ee6a10a3efbc5c62d1
Liran Tal <liran.tal@gmail.com>
2024-06-29 18:23:14 +0300

    feat: refactor to the new LangGraph based agents

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { createToolCallingAgent } from "langchain/agents";
-import { AgentExecutor } from "langchain/agents";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages";
 
 const llm = new ChatOpenAI({
   model: "gpt-3.5-turbo",
@@ -24,18 +23,11 @@ const magicTool = tool(
 );
 
 const tools = [magicTool];
-
 const query = "what is the value of magic_function(3)?";
+const app = createReactAgent({ llm, tools });
 
-const prompt = ChatPromptTemplate.fromMessages([
-  ["system", "You are a helpful assistant"],
-  ["placeholder", "{chat_history}"],
-  ["human", "{input}"],
-  ["placeholder", "{agent_scratchpad}"],
-]);
+let agentOutput = await app.invoke({
+  messages: [new HumanMessage(query)],
+});
 
-const agent = createToolCallingAgent({ llm, tools, prompt });
-const agentExecutor = new AgentExecutor({ agent, tools, verbose: true });
-
-const output = await agentExecutor.invoke({ input: query });
-console.log(output);
+console.log(agentOutput);
